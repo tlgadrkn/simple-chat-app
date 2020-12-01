c1b48619aa3436bf93983aeb5e3d73d894809c61
Tolga Durukan <tolgadurukan16@gmail.com>
2020-12-01 20:41:34 +0200

    exploring database usage

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const port = 3000;
+const mongoose = require('mongoose');
 
-const messages = [];
+const port = process.env.PORT;
+const dbURI = process.env.DB_URI;
+
+const Message = mongoose.model('messages', {
+  sender: String,
+  message: String,
+});
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -17,16 +24,28 @@ io.on('connection', () => {
 });
 
 app.get('/messages', (req, res) => {
-  console.log('from get', messages);
-  res.send(messages.map((m) => m));
+  Message.find({}, (err, data) => {
+    res.send(data);
+  });
 });
 
-app.post('/messages', (req, res) => {
-  messages.push(req.body);
-  console.log('from server post', messages);
+app.post('/messages', async (req, res) => {
+  const message = new Message(req.body);
+
+  const savedMessage = await message.save();
+  console.log('message saved');
+
   io.emit('message', req.body);
   res.sendStatus(200);
 });
+
+mongoose.connect(
+  encodeURI(dbURI),
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log('connection to db', err);
+  }
+);
 
 http.listen(port, () => {
   console.log(`app listening on port: ${port}`);
